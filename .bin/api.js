54ed28d1ab673a48c7c51b56d6f80547996b12dd
vitalii <vorobiov@artutkin.ru>
2016-05-04 15:01:24 +0300

    temp

--- .bin/api.js
+++ .bin/api.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+global.__SERVER__ = true;
 require('../server.babel');
 require(`../src/apps/${process.env.APPNAME}/server/api/app`);
--- .bin/server.js
+++ .bin/server.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 global.__CLIENT__ = false;
 global.__SERVER__ = true;
-global.__DEVELOPMENT__ = process.env.NODE_ENV === 'development';
+global.__DEVELOPMENT__ = process.env.NODE_ENV === 'dev';
 const Path = require('path');
 const appPath = `../src/apps/${ process.env.APPNAME }/server/app`;
 
 require('../server.babel');
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
+ 
+//нужно для работы с import styles from '*.scss';
 var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
 global.webpackIsomorphicTools = new WebpackIsomorphicTools(require('../webpack/webpack-isomorphic-tools'))
   .development(__DEVELOPMENT__)
